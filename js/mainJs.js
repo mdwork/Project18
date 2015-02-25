f95e0102dd8d244e0c12170a215e3309bfad2958
mdwork <mazdze@gmail.com>
2015-02-25 16:20:33 +0200

    Initial commit

---
+++ js/mainJs.js
@@ -0,0 +1,78 @@
+$(document).ready(function(){
+    /*popup function*/
+    function popupWindow(targetClick, showCurrentForm) {
+        targetClick.on('click', function (e) {
+            e.preventDefault();
+
+            var bgPopup = $('#bg-popup'),
+                fotoPopup = $('#wrap-popup');
+
+            bgPopup.addClass('show_js');
+            fotoPopup.prepend('<span class="close icon-close_js">&times;</span>')
+                .animate({'opacity': 1}, 500);
+            showCurrentForm.addClass('show_js');
+
+            bgPopup.height($(document).height());
+
+            var fotoInPopupW = fotoPopup.width(),
+                scrollTop = window.pageYOffset;
+
+            fotoPopup.css({
+                'left': '50%',
+                'margin-left': - (fotoInPopupW / 2)
+            });
+
+            $('.icon-close_js, #bg-popup').on('click', function() {
+                bgPopup.removeClass('show_js');
+                $('.icon-close_js').remove();
+                $('.header-form-callback').remove();
+                showCurrentForm.removeClass('show_js');
+
+                $(showCurrentForm).find('.show_js').removeClass('show_js');
+
+                fotoPopup.css({
+                    'opacity' : 0,
+                    'left': 0,
+                    'margin-left': 0
+                });
+            });
+
+            $("#wrap-popup").click(function(e) {
+                e.stopPropagation();
+            });
+        });
+    }
+    /*end*/
+
+    var curLinkAuthorization = $('.popup-js'),
+        curBlockShow = $('.free-ads-popup');
+
+    popupWindow(curLinkAuthorization, curBlockShow);
+
+    if ( $('#bigslider').length > 0 ) {
+        if ( $('#carousel').length > 0 ) {
+            $('#carousel').flexslider({
+                animation: "slide",
+                controlNav: false,
+                animationLoop: true,
+                slideshow: false,
+                itemWidth: 112,
+                itemMargin: 17,
+                asNavFor: '#bigslider',
+                prevText: "",
+                nextText: ""
+            });
+        }
+
+        $('#bigslider').flexslider({
+            animation: "fade",
+            controlNav: false,
+            animationLoop: true,
+            slideshow: false,
+            sync: "#carousel",
+            prevText: "",
+            nextText: ""
+        });
+        console.log('sdfs')
+    }
+});
